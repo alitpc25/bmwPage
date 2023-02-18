c261d0a5bc618a55d4243237af65a20faab8fc42
alitpc25 <alitpc2525@gmail.com>
2023-02-18 20:53:16 +0300

    swiper completed, data should be fetched

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,47 @@
-const sidebar = document.getElementById("mySidebar");  
+const sidebar = document.getElementById("mySidebar");
 
-  function openNav() {
-    sidebar.style.width = "100%";
-  }
-  
-  function closeNav() {
-    sidebar.style.width = "0";
-  }
-  
-  const parentSwiper = new Swiper('.parent-slider', {
-    // Optional parameters
-    direction: 'horizontal',
-    slidesPerView: 3,
-    spaceBetween: 18,
-    noSwiping: true,
-    centeredSlides: true,
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-      draggable: true,
-      dragSize: 50 | "auto"
-    },
-  });
-  
+function openNav() {
+  sidebar.style.width = "100%";
+}
 
-  const childSwiper = new Swiper(".child-slider", {
-    direction: 'horizontal',
-    slidesPerView: 1,
-    noSwiping: false,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
+function closeNav() {
+  sidebar.style.width = "0";
+}
+
+const parentSwiper = new Swiper('.parent-slider', {
+  // Optional parameters
+  direction: 'horizontal',
+  slidesPerView: 3,
+  spaceBetween: 18,
+  noSwiping: true,
+  centeredSlides: true,
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+    dragSize: 50 | "auto"
+  },
 });
+
+
+const childSwiper = new Swiper(".child-slider", {
+  direction: 'horizontal',
+  slidesPerView: 1,
+  noSwiping: false,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+function newImg(e){
+  const content = e.getElementsByClassName("slide-content")[0];
+  const img = content.getElementsByClassName("slide-img")[0];
+  img.src = "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (1).png"
+}
+function oldImg(e){
+  const content = e.getElementsByClassName("slide-content")[0];
+  const img = content.getElementsByClassName("slide-img")[0];
+  img.src = "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1.png"
+}

fb5e74b5b4a34305811bb14961b1f33aac480200
alitpc25 <alitpc2525@gmail.com>
2023-02-18 18:35:35 +0300

    swiper start

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,36 @@
+const sidebar = document.getElementById("mySidebar");  
+
   function openNav() {
-    document.getElementById("mySidebar").style.width = "100%";
+    sidebar.style.width = "100%";
   }
   
   function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
+    sidebar.style.width = "0";
   }
+  
+  const parentSwiper = new Swiper('.parent-slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 3,
+    spaceBetween: 18,
+    noSwiping: true,
+    centeredSlides: true,
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+      dragSize: 50 | "auto"
+    },
+  });
+  
+
+  const childSwiper = new Swiper(".child-slider", {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    noSwiping: false,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});

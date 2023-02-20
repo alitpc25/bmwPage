95c205ef1e5d44257f3ece3cd17a2a1722857664
alitpc25 <alitpc2525@gmail.com>
2023-02-20 03:08:52 +0300

    select card started

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,11 +7,21 @@ const datas = [
     types: [
       {
         type: "i4 eDrive40 – M Sport",
-        mouseOutImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1.png",
-        mouseOverImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (1).png",
-        mouseOverBackground: "linear-gradient(180deg, #A74725 0%, #5D0F05 100%)",
+        images: {
+          mobile: {
+            default: "",
+            onSelect: ""
+          },
+          web: {
+            mouseOutImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1.png",
+            mouseOverImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (1).png",
+            onSelectImage: "assets/images/g0dnnfm5dze6pud7f3qvbjvfmwld 1.png",
+          }
+        },
+        selectedBackground: "linear-gradient(180deg, #A74725 0%, #5D0F05 100%)",
         title: "Yeni BMW i4",
         text: "i4 eDrive40 - M Sport",
+        electricityOption: "All electric",
         powerOutput: {
           kW: 250,
           hp: 340
@@ -24,11 +34,21 @@ const datas = [
       },
       {
         type: "i4 M50",
-        mouseOutImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (2).png",
-        mouseOverImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 2.png",
-        mouseOverBackground: "linear-gradient(180deg, #17265E 0%, #03091C 100%)",
+        images: {
+          mobile: {
+            default: "",
+            onSelect: ""
+          },
+          web: {
+            mouseOutImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (2).png",
+            mouseOverImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 2.png",
+            onSelectImage: "",
+          }
+        },
+        selectedBackground: "linear-gradient(180deg, #17265E 0%, #03091C 100%)",
         title: "Yeni BMW i4",
         text: "i4 M50",
+        electricityOption: "All electric",
         powerOutput: {
           kW: 400,
           hp: 544
@@ -46,11 +66,21 @@ const datas = [
     types: [
       {
         type: "i7 xDrive60 M Sport",
-        mouseOutImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (4).png",
-        mouseOverImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (3).png",
-        mouseOverBackground: "linear-gradient(180deg, #A2A09B 0%, #7B7772 100%)",
+        images: {
+          mobile: {
+            default: "",
+            onSelect: ""
+          },
+          web: {
+            mouseOutImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (4).png",
+            mouseOverImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (3).png",
+            onSelectImage: "",
+          }
+        },
+        selectedBackground: "linear-gradient(180deg, #A2A09B 0%, #7B7772 100%)",
         title: "Yeni BMW i7",
         text: "i7 xDrive60 M Sport",
+        electricityOption: "All electric",
         powerOutput: {
           kW: null,
           hp: 544
@@ -67,11 +97,21 @@ const datas = [
     types: [
       {
         type: "iX1 xLine",
-        mouseOutImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (5).png",
-        mouseOverImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (6).png",
-        mouseOverBackground: "linear-gradient(180deg, #193F63 0%, #061728 100%)",
+        images: {
+          mobile: {
+            default: "",
+            onSelect: ""
+          },
+          web: {
+            mouseOutImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (5).png",
+            mouseOverImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (6).png",
+            onSelectImage: "",
+          }
+        },
+        selectedBackground: "linear-gradient(180deg, #193F63 0%, #061728 100%)",
         title: "Yeni BMW iX1",
         text: "İX1 xLine",
+        electricityOption: "All electric",
         powerOutput: {
           kW: null,
           hp: 313
@@ -88,11 +128,21 @@ const datas = [
     types: [
       {
         type: "iX3 M Sport",
-        mouseOutImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (7).png",
-        mouseOverImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (8).png",
-        mouseOverBackground: "linear-gradient(180deg, #193F63 0%, #061728 100%)",
+        images: {
+          mobile: {
+            default: "",
+            onSelect: ""
+          },
+          web: {
+            mouseOutImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (7).png",
+            mouseOverImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (8).png",
+            onSelectImage: "",
+          }
+        },
+        selectedBackground: "linear-gradient(180deg, #193F63 0%, #061728 100%)",
         title: "Yeni BMW iX3",
         text: "iX3 M Sport",
+        electricityOption: "All electric",
         powerOutput: {
           kW: null,
           hp: 313
@@ -109,11 +159,21 @@ const datas = [
     types: [
       {
         type: "iX xDrive40 M Sport",
-        mouseOutImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (9).png",
-        mouseOverImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (10).png",
-        mouseOverBackground: "linear-gradient(180deg, #5E5E5E 0%, #1A1A1A 100%)",
+        images: {
+          mobile: {
+            default: "",
+            onSelect: ""
+          },
+          web: {
+            mouseOutImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (9).png",
+            mouseOverImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (10).png",
+            onSelectImage: "",
+          }
+        },
+        selectedBackground: "linear-gradient(180deg, #5E5E5E 0%, #1A1A1A 100%)",
         title: "Yeni BMW iX",
         text: "iX xDrive40 M Sport",
+        electricityOption: "All electric",
         powerOutput: {
           kW: null,
           hp: 326
@@ -126,11 +186,21 @@ const datas = [
       },
       {
         type: "iX xDrive50 M Sport",
-        mouseOutImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (9).png",
-        mouseOverImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (11).png",
-        mouseOverBackground: "linear-gradient(180deg, #7C2F37 0%, #450109 100%)",
+        images: {
+          mobile: {
+            default: "",
+            onSelect: ""
+          },
+          web: {
+            mouseOutImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (9).png",
+            mouseOverImage: "assets/images/ssws4gz2ka14vjafolvwyneys0ac 1 (11).png",
+            onSelectImage: "",
+          }
+        },
+        selectedBackground: "linear-gradient(180deg, #7C2F37 0%, #450109 100%)",
         title: "Yeni BMW iX",
         text: "iX xDrive50 M Sport",
+        electricityOption: "All electric",
         powerOutput: {
           kW: null,
           hp: 523
@@ -156,7 +226,7 @@ const showAllModelsFunc = () => {
 
     pagination: {
       el: '.swiper-pagination',
-			clickable: true,
+      clickable: true,
     },
   });
 
@@ -189,13 +259,13 @@ const showAllModelsFunc = () => {
     document.getElementById(data.model).innerHTML =
       data.types.map(item => {
         return (`
-        <div id="${item.type}" class="swiper-slide child-swiper-slide" onmouseover="onMouseOverCard(this)" onmouseout="onMouseOutCard(this)">
+        <div id="${item.type}" class="swiper-slide child-swiper-slide" onclick="onClickCard(this)" onmouseover="onMouseOverCard(this)" onmouseout="onMouseOutCard(this)">
           <div class="slide-content">
               <div class="slide-texts">
                   <h2 class="slide-title">${item.title}</h2>
                   <p class="slide-text">${item.text}</p>
               </div>
-              <img src="${item.mouseOutImage}" class="slide-img">
+              <img src="${item.images.web.mouseOutImage}" class="slide-img">
           </div>
           <div class="select-model-button">
               <button><i><img src="assets/icons/Vector.svg"></i><span>Bu modeli seç</span></button>
@@ -217,7 +287,7 @@ const showOneModelFunc = (types) => {
 
     pagination: {
       el: '.swiper-pagination',
-			clickable: true,
+      clickable: true,
     },
   });
 
@@ -235,13 +305,13 @@ const showOneModelFunc = (types) => {
     <div class="swiper-slide parent-swiper-slide">
         <div class="swiper child-slider">
             <div id=${item.model} class="swiper-wrapper">
-              <div id="${item.type}" class="swiper-slide child-swiper-slide" onmouseover="onMouseOverCard(this)" onmouseout="onMouseOutCard(this)">
+              <div id="${item.type}" class="swiper-slide child-swiper-slide" onclick="onClickCard(this)" onmouseover="onMouseOverCard(this)" onmouseout="onMouseOutCard(this)">
                 <div class="slide-content">
                     <div class="slide-texts">
                         <h2 class="slide-title">${item.title}</h2>
                         <p class="slide-text">${item.text}</p>
                     </div>
-                    <img src="${item.mouseOutImage}" class="slide-img">
+                    <img src="${item.images.web.mouseOutImage}" class="slide-img">
                 </div>
                 <div class="select-model-button">
                     <button><i><img src="assets/icons/Vector.svg"></i><span>Bu modeli seç</span></button>
@@ -273,9 +343,10 @@ function onMouseOverCard(e) {
   img.style.width = "min(480px, 73vw)";
 
   const item = datas.filter(data => data.model === e.id.split(" ")[0])[0].types.filter(item => item.type === e.id)[0]
-  img.src = item.mouseOverImage;
-  content.style.background = item.mouseOverBackground;
+  img.src = item.images.web.mouseOverImage;
+  content.style.background = item.selectedBackground;
 }
+
 function onMouseOutCard(e) {
   const content = e.getElementsByClassName("slide-content")[0];
   const img = content.getElementsByClassName("slide-img")[0];
@@ -284,13 +355,75 @@ function onMouseOutCard(e) {
   content.style.background = "#F8F8F8";
 
   const item = datas.filter(data => data.model === e.id.split(" ")[0])[0].types.filter(item => item.type === e.id)[0]
-  img.src = item.mouseOutImage;
+  img.src = item.images.web.mouseOutImage;
 }
 
 const showModel = (e) => {
-  if(e.getElementsByTagName("a")[0].innerHTML === "All") {
+  if (document.getElementsByClassName("models-select-section")[0].style.display == "none") {
+    document.getElementsByClassName("models-select-section")[0].style.display = "block";
+  }
+  const prevActiveItem = document.getElementsByClassName("models-nav-item-active")[0];
+  prevActiveItem.classList.remove("models-nav-item-active")
+
+  e.classList.add("models-nav-item-active");
+  if (e.getElementsByTagName("a")[0].innerHTML === "All") {
     showAllModelsFunc()
     return;
   }
   showOneModelFunc(datas.filter(data => data.model === e.getElementsByTagName("a")[0].innerHTML)[0].types);
+}
+
+function onClickCard(e) {
+  document.getElementsByClassName("models-select-section")[0].style.display = "none";
+  const prevActiveItem = document.getElementsByClassName("models-nav-item-active")[0];
+  prevActiveItem.classList.remove("models-nav-item-active");
+  document.getElementById(e.id.split(" ")[0] + "-nav-button").classList.add("models-nav-item-active");
+
+  const selectedModelName = e.id;
+  console.log(selectedModelName);
+
+  const item = datas.filter(data => data.model === e.id.split(" ")[0])[0].types.filter(item => item.type === e.id)[0]
+
+  const sectionToFill = document.getElementById("model-detail-section");
+  sectionToFill.style.display = "block";
+  sectionToFill.innerHTML = `
+  <div class="model-detail-container">
+                <div class="model-detail-content">
+                    <p class="electricity-option-p">${item.electricityOption}</p>
+                    <h2>${item.type}</h2>
+                    <div class="model-detail-info d-flex">
+                        <div class="d-flex flex-column">
+                            <p class="model-detail-data-value">
+                                ${item.powerOutput.kW} kW / ${item.powerOutput.hp} bg
+                            </p>
+                            <p class="model-detail-data-name">
+                              Power output
+                            </p>
+                        </div>
+                        <div class="d-flex flex-column">
+                            <p class="model-detail-data-value">
+                              ${item.rangeInMiles.min} - ${item.rangeInMiles.max} miles
+                            </p>
+                            <p class="model-detail-data-name">
+                              Electric range WLTP
+                            </p>
+                        </div>
+                        <div class="d-flex flex-column">
+                            <p class="model-detail-data-value">
+                              ${item.milesPerKwh} mi/kWh
+                            </p>
+                            <p class="model-detail-data-name">
+                              Energy consumption WLTP
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                      <p>Discover more</p>
+                    </div>
+                </div>
+                <div class="model-detail-img">
+                    <img src="assets/images/g0dnnfm5dze6pud7f3qvbjvfmwld 1.png">
+                </div>
+            </div>`
+
 }
